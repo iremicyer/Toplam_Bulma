aa9e9560b250a03e6dd4005a0a203c9c0127f0bc
İREM İÇYER <62353089+iremicyer@users.noreply.github.com>
2020-09-01 18:53:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Toplam_Bulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double toplam = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Bir sayi girinzi:");
+                double sayi = Convert.ToDouble(Console.ReadLine());
+                toplam += sayi;
+            
+            }
+            Console.Write( "sayilarin toplami:" + toplam );
+            //Console.WriteLine(toplam);
+            Console.ReadKey();
+        }
+    }
+}
